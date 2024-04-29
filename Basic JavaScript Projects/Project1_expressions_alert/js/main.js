9a1553da99b1033ed22b905705c06805ecffb6a6
Vytenis Uld <164363165+monksop@users.noreply.github.com>
2024-04-29 14:48:08 +0100

    Add JavaScript code to demonstrate window.alert() and document.write() methods

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,17 @@
+// Utilize the window.alert() and document.write() methods
+var myString = "Hello, User!";
+window.alert(myString);
+
+// Create two variables and concatenate them
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+var concatenatedSentence = Sent1 + Sent2;
+document.write(concatenatedSentence + "<br>");
+
+// Write an expression
+var x = 10;
+var y = 5;
+var result = x + y * 2;
+
+// Display the result of the expression using document.write()
+document.write("The result of the expression x + y * 2 is: " + result + "<br>");
